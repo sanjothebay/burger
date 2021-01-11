60823df7d98c9358c43b3f14d01aec71ac9adda1
JC <sanjothebay@gmail.com>
2021-01-10 19:37:34 -0600

    added my index.handlebars and main.handlebars

---
+++ config/orm.js
@@ -0,0 +1,20 @@
+// Import MySQL connection.
+var connection = require("../config/connection.js");
+
+
+
+
+selectAll()
+
+
+insertOne()
+
+
+updateOne()
+
+
+
+
+
+// Export the orm object for the model (cat.js).
+module.exports = orm;
