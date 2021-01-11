966fdc82dd6119c1bbec0170fc352adce1817004
JC <sanjothebay@gmail.com>
2021-01-10 19:16:59 -0600

    added my seed.sql

---
+++ db/seed.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUE ("Double Double Burger");
+INSERT INTO burgers (burger_name) VALUE ("4 X Burger");
+INSERT INTO burgers (burger_name, devoured) VALUE ("Cheese Burger", true );
+INSERT INTO burgers (burger_name, devoured) VALUE ("Kids Burger", true );
+
+SELECT * FROM burgers
