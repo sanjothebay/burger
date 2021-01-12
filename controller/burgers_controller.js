cd43955da43cfd1e346519c582a8f1d4f082ac59
JC <sanjothebay@gmail.com>
2021-01-11 22:24:41 -0600

    forgot to save after each function

--- controller/burgers_controller.js
+++ controller/burgers_controller.js
@@ -2,13 +2,25 @@ var express = require("express");
 var router = express.Router();
 var burger = require("../models/burger.js");
 
+router.get("/", (req, res) => {
+  burger.selectAll((data) => {
+    var hbsObject = {
+      burger: data,
+    };
+    res.render("index", hbsObject);
+  });
+});
 
+router.post("/api/burgers", (req, res) => {
+  burger.insertOne(
+    ["name", "devoured"],
+    [req.body.name, req.body.devoured],
+    (result) => {
+      res.json({ id: result.instertid });
+    }
+  );
+});
 
-
-
-
-
-
-
+router.put("api/burger/:id", )
 // Export routes for server.js to use.
 module.exports = router;
