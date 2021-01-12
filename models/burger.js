a1b95fc6116d3124960633767db3a5424ea53b2c
JC <sanjothebay@gmail.com>
2021-01-11 22:08:48 -0600

    added my function

--- models/burger.js
+++ models/burger.js
@@ -1,16 +1,23 @@
-// // Import the ORM to create functions that will interact with the database.
-// var orm = require("../config/orm.js");
+// Import the ORM to create functions that will interact with the database.
+var orm = require("../config/orm.js");
 
+var burger = {
+  selectAll: (cb) => {
+    orm.selectAll("burgers", (res) => {
+      cb(res);
+    });
+  },
+  insertOne: (cols, vals, cb) => {
+    orm.insertOne("burgers", cols, vals, (res) => {
+      cb(res);
+    });
+  },
+  updateOne: (objColVals, condition, cb) => {
+    orm.updateOne("burger", objColVals, condition, (res) => {
+      cb(res);
+    });
+  },
+};
 
-
-
-
-
-
-
-
-
-
-
-// // Export the database functions for the controller (burgersController.js).
-// module.exports = burger;
+// Export the database functions for the controller (burgersController.js).
+module.exports = burger;
